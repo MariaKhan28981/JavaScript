3c82fbf7b69559421788169e2eb94ee6deaa66d1
Maria Khan <Mariakhan200502@gmail.com>
2025-11-29 17:48:31 +0000

    data_types

---
+++ basics/data_types.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+// we are using node js not browser that is why it will throw an error -> alert not found ,alert("hello")
+let name="maria"
+let age=20
+let isLoggedIn=false
+/*
+number=> 2 to power 53
+bigint
+string=>""
+boolean=> true/false
+null=>standlaone value
+undefined
+symbol=> unique */
+
+//object
+
+console.log(typeof "maria")
+console.log(typeof age)
+console.log(typeof null) //object
