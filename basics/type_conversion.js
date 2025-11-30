f282f6762c6cd5215280201fdf5516cb91895086
Maria Khan <Mariakhan200502@gmail.com>
2025-11-30 17:53:30 +0000

    conversion of data types

---
+++ basics/type_conversion.js
@@ -0,0 +1,36 @@
+//let score=33
+//console.log(typeof score)
+
+//let Score="33"
+//console.log(typeof Score)
+
+//let valueInNumber = Number(Score) //N must be capital
+//console.log(typeof valueInNumber)
+
+//let a= "123abc"
+//let a_as_number= Number(a)
+//console.log(typeof a_as_number)
+//console.log(a_as_number)
+
+//let b=null
+//console.log(typeof b)
+//console.log(b)
+
+//let c= true
+//console.log(typeof c)
+//console.log(c)
+//let c_num = Number(c)
+//console.log(typeof c)
+//console.log(c_num)
+//"33"->33
+//"33abc"-> NaN
+//"true"->1; false->0
+let isLoggedIn= 1
+let bool_isLoggedIn= Boolean(isLoggedIn)
+console.log(bool_isLoggedIn)
+//1-> true;0->false
+//""->false
+//"maria"->true
+let some=33
+let num_some= String(some)
+console.log(typeof num_some)
