e8add78dca6f7df92a1c6ba3a04e24f048d4cfee
Maria Khan <Mariakhan200502@gmail.com>
2025-12-19 11:14:38 +0000

    scopes

--- 04_function/03_scopes.js
+++ 04_function/03_scopes.js
@@ -1,7 +1,12 @@
 //let //const //var
-let a=10
-const b=20
-var c=30
-{
-    
+var c=300
+let a=900 // this will be printed instead of 30
+if (true){
+    let a=10
+    const b=20
+    var c=30 // same as c=30
 }
+// console.log(a) // a is not defined
+// console.log(b) // b is not defined
+console.log(c) // 30
+    
