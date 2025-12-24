91683ac34e3955dd666fd3c4d06ca208c6e909a4
Maria Khan <Mariakhan200502@gmail.com>
2025-12-24 16:42:17 +0000

    learning control flow

---
+++ 05_control_flow/01.js
@@ -0,0 +1,23 @@
+const isUserLoggedIn=true
+const debit_card=true
+// if(isUserLoggedIn){
+//     console.log("succesful")
+// }else{
+
+// }
+// // <,>,<=,>=,==,!=,===,!==
+ 
+
+//short hand notation
+const balance=600
+if (balance>500) console.log("test1"),console.log("test2") //aise nhi krna chahiye
+
+
+if(isUserLoggedIn && debit_card){ // || for OR
+    console.log("successfull")
+
+}else if(){
+
+}else (){
+
+}
---
+++ 05_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month=3
+switch (month) {
+    //shift+alt+down arrow -> for duplicate
+    case 1: // case "jan"
+        console.log("January")
+        break;
+    case 2:
+        console.log("Febraury")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    default:
+        console.log("invalid")
+        break;
+}
